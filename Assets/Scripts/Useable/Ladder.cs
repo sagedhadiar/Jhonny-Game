942bc067bc9966e073cb71b73bd537552cd9f62d
saged <sagedhaidar94@gmail.com>
2018-05-31 21:31:36 +0300

    UG-40 Jumping off

--- Assets/Scripts/Useable/Ladder.cs
+++ Assets/Scripts/Useable/Ladder.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ladder : MonoBehaviour, IUseable {
-  
+
+    [SerializeField]
+    private Collider2D platformCollider;
 
     // Use this for initialization
     void Start () {
@@ -16,6 +18,30 @@ public class Ladder : MonoBehaviour, IUseable {
 	}
 
     public void Use() {
-        PlayerController.Instance.OnLadder = true;
+
+        if(PlayerController.Instance.OnLadder) {
+            // we need to stop climbing
+            UseLadder(false, 1);
+        }
+        else {
+            // we need to start climbing
+            UseLadder(true, 0);
+            Physics2D.IgnoreCollision(PlayerController.Instance.GetComponent<Collider2D>(), platformCollider, true);
+        }
+    }
+
+    private void UseLadder(bool onLadder, int gravity) {
+
+        PlayerController.Instance.OnLadder = onLadder;
+        PlayerController.Instance.MyRigidBody.gravityScale = gravity;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+
+        if (other.tag == "jhonny")
+        {
+            UseLadder(false, 1);
+            Physics2D.IgnoreCollision(PlayerController.Instance.GetComponent<Collider2D>(), platformCollider, false);
+        }
     }
 }
